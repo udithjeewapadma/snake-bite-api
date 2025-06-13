474bd457c866526d2bca96df8ed7aa6f5fe19856
Jeewapadma <udithjeewapadma117@gmail.com>
2025-06-13 13:11:24 +0530

    add validations

--- src/main/java/com/example/snake_bite_api/controller/dto/request/CreateAdminRequestDTO.java
+++ src/main/java/com/example/snake_bite_api/controller/dto/request/CreateAdminRequestDTO.java
@@ -1,11 +1,24 @@
 package com.example.snake_bite_api.controller.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CreateAdminRequestDTO {
 
+    @NotBlank(message = "User name cannot be blank")
     private String adminName;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
     private String email;
-    private int phoneNumber;
+
+    @NotBlank(message = "Phone number cannot be blank")
+    @Pattern(regexp = "\\d{10}", message = "Phone number must contain exactly 10 digits")
+    private String phoneNumber;
 }
