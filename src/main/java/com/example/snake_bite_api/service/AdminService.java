261bfb8052e26f2718cbaba08f59c11c626abe6b
Jeewapadma <udithjeewapadma117@gmail.com>
2025-06-13 13:26:28 +0530

    implementing findAdminById method in adminServiceImpl

--- src/main/java/com/example/snake_bite_api/service/AdminService.java
+++ src/main/java/com/example/snake_bite_api/service/AdminService.java
@@ -1,9 +1,12 @@
 package com.example.snake_bite_api.service;
 
 import com.example.snake_bite_api.controller.dto.request.CreateAdminRequestDTO;
+import com.example.snake_bite_api.controller.dto.response.AdminResponseDTO;
 import com.example.snake_bite_api.models.Admin;
 
 public interface AdminService {
 
     Admin createAdmin(CreateAdminRequestDTO createAdminRequestDTO);
+
+    AdminResponseDTO findAdminById(Long id);
 }
--- src/main/java/com/example/snake_bite_api/service/impl/AdminServiceImpl.java
+++ src/main/java/com/example/snake_bite_api/service/impl/AdminServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.snake_bite_api.service.impl;
 
 import com.example.snake_bite_api.controller.dto.request.CreateAdminRequestDTO;
+import com.example.snake_bite_api.controller.dto.response.AdminResponseDTO;
+import com.example.snake_bite_api.exception.AdminNotFoundException;
 import com.example.snake_bite_api.models.Admin;
 import com.example.snake_bite_api.repository.AdminRepository;
 import com.example.snake_bite_api.service.AdminService;
@@ -20,5 +22,17 @@ public class AdminServiceImpl implements AdminService {
         admin.setEmail(createAdminRequestDTO.getEmail());
         admin.setPhoneNumber(createAdminRequestDTO.getPhoneNumber());
         return adminRepository.save(admin);
+    }
+
+    @Override
+    public AdminResponseDTO findAdminById(Long id) {
+        Admin admin = adminRepository.findById(id)
+                .orElseThrow(() -> new AdminNotFoundException("Admin not found"));
+        AdminResponseDTO adminResponseDTO = new AdminResponseDTO();
+        adminResponseDTO.setId(admin.getId());
+        adminResponseDTO.setAdminName(admin.getAdminName());
+        adminResponseDTO.setEmail(admin.getEmail());
+        adminResponseDTO.setPhoneNumber(admin.getPhoneNumber());
+        return adminResponseDTO;
     }
 }
