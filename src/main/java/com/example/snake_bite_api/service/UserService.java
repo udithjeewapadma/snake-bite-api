e16e79a7271718fd509951bd7adfa4586a0a28c2
Jeewapadma <udithjeewapadma117@gmail.com>
2025-06-13 10:14:46 +0530

    implementing findUserById method in userServiceImpl

--- src/main/java/com/example/snake_bite_api/service/UserService.java
+++ src/main/java/com/example/snake_bite_api/service/UserService.java
@@ -1,9 +1,12 @@
 package com.example.snake_bite_api.service;
 
 import com.example.snake_bite_api.controller.dto.request.CreateUserRequestDTO;
+import com.example.snake_bite_api.controller.dto.response.UserResponseDTO;
 import com.example.snake_bite_api.models.User;
 
 public interface UserService {
 
     User createUser(CreateUserRequestDTO createUserRequestDTO);
+
+    UserResponseDTO findUserById(Long id);
 }
--- src/main/java/com/example/snake_bite_api/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/snake_bite_api/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.snake_bite_api.service.impl;
 
 import com.example.snake_bite_api.controller.dto.request.CreateUserRequestDTO;
+import com.example.snake_bite_api.controller.dto.response.UserResponseDTO;
+import com.example.snake_bite_api.exception.UserNotFoundException;
 import com.example.snake_bite_api.models.User;
 import com.example.snake_bite_api.repository.UserRepository;
 import com.example.snake_bite_api.service.UserService;
@@ -22,5 +24,18 @@ public class UserServiceImpl implements UserService {
         user.setPhoneNumber(createUserRequestDTO.getPhoneNumber());
 
         return userRepository.save(user);
+    }
+
+    @Override
+    public UserResponseDTO findUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+        userResponseDTO.setId(user.getId());
+        userResponseDTO.setUserName(user.getUserName());
+        userResponseDTO.setEmail(user.getEmail());
+        userResponseDTO.setPhoneNumber(user.getPhoneNumber());
+        return userResponseDTO;
+
     }
 }
