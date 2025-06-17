2dc3d380796562582f4cdf7359ef7f4ae9dd7cd2
Jeewapadma <udithjeewapadma117@gmail.com>
2025-06-17 11:33:56 +0530

    doing some updates and implementing postMapping to createBlog

---
+++ src/main/java/com/example/snake_bite_api/controller/BlogController.java
@@ -0,0 +1,25 @@
+package com.example.snake_bite_api.controller;
+
+import com.example.snake_bite_api.controller.dto.request.CreateBlogRequestDTO;
+import com.example.snake_bite_api.controller.dto.response.BlogResponseDTO;
+import com.example.snake_bite_api.models.Blog;
+import com.example.snake_bite_api.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/blogs")
+public class BlogController {
+
+    @Autowired
+    private BlogService blogService;
+
+    @PostMapping
+    public BlogResponseDTO createBlog(@RequestParam Long userId,
+                                      @ModelAttribute CreateBlogRequestDTO createBlogRequestDTO)
+            throws IOException {
+        return blogService.createBlog(userId,createBlogRequestDTO);
+    }
+}
--- src/main/java/com/example/snake_bite_api/models/Blog.java
+++ src/main/java/com/example/snake_bite_api/models/Blog.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class Blog {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
