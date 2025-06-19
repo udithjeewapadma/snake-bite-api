05c9d01e9da39826757d9fb2d69f8180795590a7
Jeewapadma <udithjeewapadma117@gmail.com>
2025-06-19 10:16:51 +0530

    implementing createComment method in controller

---
+++ src/main/java/com/example/snake_bite_api/controller/CommentController.java
@@ -0,0 +1,28 @@
+package com.example.snake_bite_api.controller;
+
+import com.example.snake_bite_api.controller.dto.request.CreateCommentRequestDTO;
+import com.example.snake_bite_api.controller.dto.response.CommentResponseDTO;
+import com.example.snake_bite_api.models.Comment;
+import com.example.snake_bite_api.service.CommentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/comments")
+@AllArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CommentResponseDTO createComment(@RequestParam Long userId, @RequestBody CreateCommentRequestDTO createCommentRequestDTO) {
+        Comment comment = commentService.createComment(userId, createCommentRequestDTO);
+        CommentResponseDTO commentResponseDTO = new CommentResponseDTO();
+        commentResponseDTO.setId(comment.getId());
+        commentResponseDTO.setComment(comment.getComment());
+        commentResponseDTO.setUserId(comment.getUser().getId());
+        return commentResponseDTO;
+    }
+}
