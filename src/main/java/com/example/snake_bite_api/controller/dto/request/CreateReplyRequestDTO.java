164d2056b2857f34a4b684b6e074fcd883580bc6
Jeewapadma <udithjeewapadma117@gmail.com>
2025-06-23 11:22:22 +0530

    implementing request and response dtos for reply model

---
+++ src/main/java/com/example/snake_bite_api/controller/dto/request/CreateReplyRequestDTO.java
@@ -0,0 +1,9 @@
+package com.example.snake_bite_api.controller.dto.request;
+
+import lombok.Data;
+
+@Data
+public class CreateReplyRequestDTO {
+
+    private String replyContent;
+}
---
+++ src/main/java/com/example/snake_bite_api/controller/dto/response/ReplyResponseDTO.java
@@ -0,0 +1,14 @@
+package com.example.snake_bite_api.controller.dto.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ReplyResponseDTO {
+
+    private Long Id;
+    private String replyContent;
+    private Long userId;
+    private Long commentId;
+}
