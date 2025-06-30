9d1aa71321284ec4ff7808c5da21281e54b702ec
Jeewapadma <udithjeewapadma117@gmail.com>
2025-06-30 09:39:28 +0530

    update RequestNewSnake model

--- src/main/java/com/example/snake_bite_api/models/RequestNewSnake.java
+++ src/main/java/com/example/snake_bite_api/models/RequestNewSnake.java
@@ -3,15 +3,28 @@ package com.example.snake_bite_api.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "newSnakes")
 public class RequestNewSnake {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String description;
+    private String species;
+    private String color;
+    private String pattern;
+    private double averageLength;
+
+    @Enumerated(EnumType.STRING)
+    private Venomous venomous;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> imageUrl;
+
 
     @Enumerated(EnumType.STRING)
     private SnakeRequestStatus status;
