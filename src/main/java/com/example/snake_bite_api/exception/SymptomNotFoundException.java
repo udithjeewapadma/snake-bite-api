59d12e76e45e27e21f53102ee0ec97429e41c9af
Jeewapadma <udithjeewapadma117@gmail.com>
2025-07-04 09:46:51 +0530

    implementing excption and repository to symptom

---
+++ src/main/java/com/example/snake_bite_api/exception/SymptomNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.snake_bite_api.exception;
+
+public class SymptomNotFoundException extends RuntimeException {
+    public SymptomNotFoundException(String message) {
+        super(message);
+    }
+}
