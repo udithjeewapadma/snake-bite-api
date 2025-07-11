722d7086216ce66454aa048b668356ac3b886b12
Jeewapadma <udithjeewapadma117@gmail.com>
2025-07-11 10:38:25 +0530

    implementing exception and repository for firstAid

---
+++ src/main/java/com/example/snake_bite_api/exception/FirstAidNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.snake_bite_api.exception;
+
+public class FirstAidNotFoundException extends RuntimeException {
+  public FirstAidNotFoundException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/example/snake_bite_api/repository/FirstAidRepository.java
@@ -0,0 +1,7 @@
+package com.example.snake_bite_api.repository;
+
+import com.example.snake_bite_api.models.FirstAid;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FirstAidRepository extends JpaRepository<FirstAid, Long> {
+}
